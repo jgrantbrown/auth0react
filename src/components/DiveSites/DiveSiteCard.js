adec6e838df4fdb0e29a8073fdd075ef9bc700da
Jason <j.grant.brown@gmail.com>
2019-05-14 11:38:33 -0400

    start styling for divesite cards

--- src/components/DiveSites/DiveSiteCard.js
+++ src/components/DiveSites/DiveSiteCard.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-
-
+import "./DiveSiteCard.css"
 class DiveSiteCard extends Component {
 
   render(){
-    return (<div key={this.props.dive.id}>
+    return (<div className="DiveSiteCard" key={this.props.dive.id}>
                 <p>{this.props.dive.name}  Located:{this.props.dive.location}</p>
                 <p>Description: {this.props.dive.description}</p>
             </div>
