6fa355cba04763d3df626ce9f4950f77ec807e2a
Jason <j.grant.brown@gmail.com>
2019-05-30 15:30:22 -0400

    making api request for divesites is active

--- src/components/DiveSites/DiveSites.js
+++ src/components/DiveSites/DiveSites.js
@@ -4,43 +4,36 @@ import { Card , CardGroup} from 'react-bootstrap';
 import "./DiveSites.css"
 import {Link} from "react-router-dom";
 
-let divesites = [{id: 1, name:"The Blue Hole", location:"Belize", description:"BLAH BLAH"},{id: 2 ,name:"Aquarium", location: "Provenciales", description:"BLAH BLAH"},{id: 3 ,name:"The Wall", location: "Saba", description:"BLAH BLAH"}];
-let diveInfo = divesites.map(dive=>{
-                return  <Card key={dive.id} style={{ width: '18rem' }}>
-                          <DiveSiteCard dive={dive} />
-                        </Card>
-                });
-
-
 class DiveSites extends Component {
   constructor(props) {
       super(props);
     this.state ={
-      divesites1: [],
+      divesites: [],
     };
   }
 
   // Make call to api for list of dive sites
-  componentWillMount(){
+componentWillMount(){
       const url = "http://localhost:3000/api/divesites"
       fetch(url)
       .then(res => res.json())
       .then(result =>  this.setState({
-          divesites1: result
+          divesites: result
       }) )
       .catch(() => console.log("Can't access " + url + " response. Blocked by browser"))
 }
-
 
   render(){
   return (<div>
       List of Dive sites Here
       <CardGroup>
-        {diveInfo}
+        {this.state.divesites.map(dive =>(
+          <Card key={dive.id} style={{ width: '18rem' }}>
+              <DiveSiteCard dive={dive} />
+          </Card>
+        ))}
       </CardGroup>
-      {this.state.divesites1.map(dive =>(
-        <li key={dive.id}>{dive.name}</li>
-      ))}
+
     </div>
   );
 }
