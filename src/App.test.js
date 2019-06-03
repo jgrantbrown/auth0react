09965247a92626ccef43098265bbe2e8d9a90b13
Jason <j.grant.brown@gmail.com>
2019-06-03 08:54:18 -0400

    added mocha testing file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<App />);
+    const welcome = <p>Welcome to  ScubaBook  </p>;
+    expect(wrapper.contains(welcome)).to.equal(true);
+  });
 });
